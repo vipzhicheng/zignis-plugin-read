313d9031c0f96eb5fd05f102ef9ef53ca739f61f
Richard Yu <vipzhicheng@gmail.com>
2019-10-15 02:34:42 +0800

    init

---
+++ src/commands/read.ts
@@ -0,0 +1,34 @@
+
+import util from 'util'
+import fs from 'fs'
+import read from 'readability-js'
+import TurndownService from 'turndown'
+
+const promiseRead = util.promisify(read)
+const turndownService = new TurndownService()
+export const disabled = false // Set to true to disable this command temporarily
+export const command = 'read <url>'
+export const desc = 'read main part of a page'
+// export const aliases = ''
+// export const middleware = (argv) => {}
+
+export const builder = function (yargs: any) {
+  yargs.option('title', { default: true, describe: 'prepend title, disable use no-title', alias: 'T' })
+  yargs.option('footer', { default: true, describe: 'append footer, disable use no-footer', alias: 'F' })
+}
+
+export const handler = async function (argv: any) {
+  const article = await promiseRead(argv.url)
+  let content = article.content.html()
+  if (argv.title) {
+    content = `<h1>${article.title}</h1>\n\n${content}`
+  }
+
+  let markdown = turndownService.turndown(content)
+  if (argv.footer) {
+    markdown = `${markdown}\n\n---\n\nFrom: ${argv.url}`
+  }
+
+  fs.writeFileSync(article.title + '.md', markdown)
+  console.log('Done!')
+}
