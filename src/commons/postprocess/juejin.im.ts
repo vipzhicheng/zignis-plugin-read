922592075953e1f972b1cc98ae1380eaf5d55085
Richard Yu <vipzhicheng@gmail.com>
2019-10-30 11:52:14 +0800

    add postprocess rule

--- src/commons/postprocess/juejin.im.ts
+++ src/commons/postprocess/juejin.im.ts
@@ -3,6 +3,14 @@ export default (markdown, opts) => {
   // 去掉网页复制代码功能带来的干扰
   markdown = markdown.replace(/\s+复制代码\n/g, '')
 
+  // 过滤掉锚点里的外链
+  markdown = markdown.replace(/\[(.*?)\]\((.*?)#(.*?)\)/g, (match, p1, p2, p3) => {
+    if (p2 === opts.url) {
+      return `[${p1}](#${p3})`
+    }
+    return match
+  })
+
   // 加上本地代理，反防盗链
   if (opts.format === 'web') {
     markdown = markdown.replace(/\!\[(.*?)\]\((.*?)\)/g,  (match, p1, p2) => {
