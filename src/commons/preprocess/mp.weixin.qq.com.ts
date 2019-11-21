d2db101e16a12ca60b350e17c0fbb5ef7d345fcf
Richard Yu <vipzhicheng@gmail.com>
2019-11-21 20:13:23 +0800

    adjust mp process

--- src/commons/preprocess/mp.weixin.qq.com.ts
+++ src/commons/preprocess/mp.weixin.qq.com.ts
@@ -4,10 +4,10 @@ export default (html: string, argv) => {
   html = html.replace(/<pre[\s\S]*?>[\s\S]*?<\/pre>/g, (match1) => {
     match1 = match1.replace(/<code[\s\S]*?>[\s\S]*?<span[\s\S]*?>([\s\S]*?)<\/span>[\s\S]*?<\/code>/g, (match2, p1) => {
       p1 = p1.replace(/<br.*?\/?>/g, '')
-      return `    ${p1}\n`
+      return `${p1}\n`
     })
 
-    return match1
+    return `<pre><code>${match1}</code></pre>`
   })
 
   return html
